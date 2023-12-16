5b54030f85858ea2f1fc96cafd66cb3935b7635b
Maxim Kucherenko <maximkucherenkowork@gmail.com>
2023-12-16 17:38:44 +0200

    [feature] Blog: Add pagination

--- web/app/themes/test-theme/index.php
+++ web/app/themes/test-theme/index.php
@@ -22,57 +22,62 @@ foreach ($categories as $category) {
         </div>
         <div class="p-1.5 mb-16 mx-auto w-full max-w-max border border-black/10 bg-white rounded-md flex items-center justify-center flex-wrap gap-2">
             <div>
-                <?php if (is_home()) { ?>
+                <?php if (is_home()) : ?>
                     <a class="inline-block py-2 px-4 bg-[#444BFF] text-white rounded-md transition" href="<?php the_permalink(get_option('page_for_posts')); ?>">
                         <span>All</span>
                     </a>
-                <?php } else { ?>
+                <?php else : ?>
                     <a class="inline-block py-2 px-4 text-[#111111] rounded-md hover:bg-[#444BFF]/10 transition" href="<?php the_permalink(get_option('page_for_posts')); ?>">
                         <span>All</span>
                     </a>
-                <?php } ?>
+                <?php endif; ?>
             </div>
-            <?php foreach ($categories_array as $category) { ?>
+            <?php foreach ($categories_array as $category) : ?>
                 <div>
-                    <?php if ($category['active']) { ?>
+                    <?php if ($category['active']) : ?>
                         <a class="inline-block py-2 px-4 text-white bg-[#444BFF] rounded-md transition" href="<?php echo $category['url'] ?>">
                             <span><?php echo $category['title'] ?></span>
                         </a>
-                    <?php } else { ?>
+                    <?php else : ?>
                         <a class="inline-block py-2 px-4 text-[#111111] rounded-md hover:bg-[#444BFF]/10 transition" href="<?php echo $category['url'] ?>">
                             <span><?php echo $category['title'] ?></span>
                         </a>
-                    <?php } ?>
+                    <?php endif; ?>
                 </div>
-            <?php } ?>
+            <?php endforeach; ?>
         </div>
 
-        <?php if (have_posts()) { ?>
+        <?php if (have_posts()) : ?>
             <div class="grid sm:grid-cols-2 lg:grid-cols-3 items-start gap-8">
-                <?php while (have_posts()) {
+                <?php while (have_posts()) :
                     the_post(); ?>
 
                     <div class="group relative bg-white border border-black/10 rounded-md overflow-hidden">
-                        <div class="[&_img]:w-full"><?php the_post_thumbnail(); ?></div>
+                        <div class="w-full aspect-h-9 aspect-w-16 [&_img]:object-cover"><?php the_post_thumbnail(); ?></div>
                         <div class="p-6">
                             <div class="mb-4 flex items-center gap-3">
                                 <span class="inline-block py-1 px-2 bg-[#111111] text-sm text-white rounded"><?php the_category() ?></span>
                                 <span class="inline-block text-sm text-[#111111]"><?php echo get_the_date('M d, Y') ?></span>
                             </div>
                             <h3 class="text-2xl text-[#111111] !leading-normal font-bold group-hover:underline"><?php the_title() ?></h3>
-                            <!-- <div class="mb-6 text-base tracking-wide text-[#111111]/60">
-                                <?php the_content() ?>
-                            </div>
-                            <span class="inline-block py-2 px-4 border border-[#111111] text-[#111111] rounded">Read more</span> -->
                         </div>
                         <a class="absolute inset-0" href="<?php the_permalink(); ?>"></a>
                     </div>
-
-                <?php } ?>
+                <?php endwhile; ?>
             </div>
-        <?php } else {
+
+            <?php if (!empty(get_the_posts_pagination())) : ?>
+                <div class="pt-10 md:pt-20 [&_div]:flex [&_div]:justify-center [&_div]:items-center [&_div]:gap-4 [&_span]:inline-block [&_span]:py-2 [&_span]:px-4 [&_span]:bg-[#444BFF] [&_span]:text-white [&_span]:rounded-md [&_a]:inline-block [&_a]:py-2 [&_a]:px-4 [&_a]:text-[#111111] [&_a]:border [&_a]:border-black/10 [&_a]:rounded-md hover:[&_a]:bg-[#444BFF]/10 [&_a]:transition">
+                    <?php the_posts_pagination([
+                        'prev_text' => '<',
+                        'next_text' => '>',
+                    ]); ?>
+                </div>
+            <?php endif; ?>
+
+        <?php else :
             echo '<p>There is no posts</p>';
-        } ?>
+        endif; ?>
     </div>
 </div>
 
--- web/app/themes/test-theme/single.php
+++ web/app/themes/test-theme/single.php
@@ -9,7 +9,7 @@
                 <span class="block mb-5 font-semibold text-[#111111] tracking-wider">
                     <a class="text-[#444BFF] hover:underline" href="<?php the_permalink(get_option('page_for_posts')); ?>">Blog</a> / <?php echo the_title() ?>
                 </span>
-                <div class="mb-4 w-full aspect-h-1 aspect-w-2 [&_img]:object-cover [&_img]:border [&_img]:border-black/10 [&_img]:rounded-md [&_img]:overflow-hidden"><?php the_post_thumbnail() ?></div>
+                <div class="mb-4 w-full aspect-h-9 aspect-w-16 [&_img]:object-cover [&_img]:border [&_img]:border-black/10 [&_img]:rounded-md [&_img]:overflow-hidden"><?php the_post_thumbnail() ?></div>
                 <h2 class="mb-2 text-3xl md:text-4xl text-[#111111] !leading-normal font-bold group-hover:underline"><?php the_title() ?></h2>
                 <div class="flex items-center gap-3">
                     <span class="inline-block py-1 px-2 bg-[#111111] text-sm text-white rounded"><?php the_category() ?></span>
@@ -19,6 +19,15 @@
         </div>
 
         <?php the_content(); ?>
+
+        <div class="mx-auto py-14 md:py-10 px-4 max-w-4xl flex justify-center items-center gap-5 [&_a]:py-2 [&_a]:px-4 [&_a]:border [&_a]:border-black/10 [&_a]:text-base [&_a]:text-[#111111] [&_a]:rounded-md hover:[&_a]:bg-[#444BFF]/10 [&_a]:transition">
+            <?php
+
+            previous_post_link('%link', '<-- Prev Post');
+            next_post_link('%link', 'Next Post -->');
+
+            ?>
+        </div>
     </div>
 <?php } ?>
 
